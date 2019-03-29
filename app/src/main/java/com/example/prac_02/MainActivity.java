f399bc60db4133c992d83d0fedc0d557e13ed7c5
Cyra25 <44994077+Cyra25@users.noreply.github.com>
2019-03-29 13:52:12 +0800

    commit3

--- app/src/main/java/com/example/prac_02/MainActivity.java
+++ app/src/main/java/com/example/prac_02/MainActivity.java
@@ -2,12 +2,53 @@ package com.example.prac_02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView statusText;
+    private EditText guessNumber;
+    private final static int secretValue = 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        statusText = (TextView)findViewById(R.id.statusText);
+
+        guessNumber = (EditText) findViewById(R.id.guessNumber);
+        guessNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                statusText.setText(getString(R.string.hello));
+            }
+
+            @Override
+            public void onTextChanged(CharSequence string, int start, int before, int count) {
+                try {
+                    int value = Integer.parseInt(string.toString());
+                    if (value == secretValue) {
+                        statusText.setText(getString(R.string.win));
+                    } else if (value < secretValue) {
+                        statusText.setText(getString(R.string.small));
+                    } else if (value > secretValue) {
+                        statusText.setText(getString(R.string.big));
+                    }
+                }catch (Exception e){
+                    //
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
+
 }
+
---
+++ app/src/main/java/com/example/prac_02/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.prac_02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+}
